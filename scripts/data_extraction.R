5a8ff3a766632a47085d9dad90acbc781537b943
rpecchia <rpecchia@gmail.com>
2019-11-12 11:26:40 -0800

    first upload of scripts for UMD meta-analysis

---
+++ scripts/data_extraction.R
@@ -0,0 +1,2 @@
+library(metaDigitise)
+metaDigitise(dir = "~/Desktop/UMD_org_soil_MA/data_in_figures/", summary = TRUE)
---
+++ scripts/figure_map_of_study_sites.R
@@ -0,0 +1,4 @@
+## load data ####
+source("~/Desktop/UMD_org_soil_MA/UMD_project/scripts/soil_health_clean_data.R")
+
+## load libraries ####
