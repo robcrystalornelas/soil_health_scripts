2bdc0562b59ce0e5ead7c739bbdd3642018f8be2
Rob Crystal-Ornelas <robcrystalornelas@Robs-MacBook-Pro.local>
2019-12-03 08:41:10 -0800

    new clean data script separating out SOC and MBC

--- scripts/soil_health_clean_data.R
+++ scripts/soil_health_clean_data.R
@@ -1,5 +1,8 @@
 ## Load libraries ####
+library(tidyverse)
 
 ## read in data ####
-soil_health_raw_data <- read.csv("~/Desktop/research/UMD_org_soil_MA/Crystal-Ornelas_Thapa_Tully_datasheet - datasheet.csv", header = TRUE)
-head(soil_health_raw_data)
+soil_health_raw_data <- read.csv("~/Desktop/research/UMD_org_soil_MA/Crystal-Ornelas_Thapa_Tully_datasheet - datasheet_v2.csv", header = TRUE)
+
+raw_data_SOC <- filter(soil_health_raw_data, outcome_standardized == "SOC (g/kg)") 
+raw_data_MBC <- filter(soil_health_raw_data, outcome_standardized == "MBC (ug/g)")
