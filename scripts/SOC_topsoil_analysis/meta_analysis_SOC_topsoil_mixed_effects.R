4418b4c38c054f8025df61c4cf49e4f8263a61b0
Robert Crystal-Ornelas <rcrystalornelas@usfca.edu>
2020-02-07 10:52:26 -0800

    new forest plots with better labels for the studies

--- scripts/SOC_topsoil_analysis/meta_analysis_SOC_topsoil_mixed_effects.R
+++ scripts/SOC_topsoil_analysis/meta_analysis_SOC_topsoil_mixed_effects.R
@@ -53,3 +53,39 @@ forest(mixed_effects_SOC_topsoil,
        cex = .8,
        col = "#F66B4D",
        mlab = "Summary")
+
+# This shows vector of ordering by focal organic system
+order(effect_sizes_SOC_topsoil$focal_organic_system)
+# Forest plot grouped
+study_numbers <- c(1:116)
+plyr::count(effect_sizes_SOC_topsoil$focal_organic_system)
+
+full_ma_study_label <- c(
+  "biochar",
+  strrep("", 1:1),
+  "cover crop",
+  strrep("", 1:6),
+  "crop rotation diversity",
+  strrep("", 1:1),
+  "green manure",
+  strrep("", 1:3),
+  "green manure and organic amendment",
+  strrep("", 1:1),
+  "organic amendment",
+  strrep("", 1:49),
+  "organic amendment and till",
+  strrep("", 1:3),
+  "organic amendment and till and cover crop",
+  strrep("", 1:32),
+  "tillage",
+  strrep("", 1:11))
+full_ma_study_label
+
+forest(mixed_effects_SOC_topsoil,
+       annotate = FALSE,
+       xlab = "ln(Response Ratio)",
+       slab = full_ma_study_label,
+       cex = .9,
+       col = "#F66B4D",
+       mlab = "Summary",
+       order = order(effect_sizes_SOC_topsoil$focal_organic_system))
--- scripts/general_descriptives/figure_map_of_study_sites.R
+++ scripts/general_descriptives/figure_map_of_study_sites.R
@@ -1,12 +1,10 @@
 ## load data ####
-
 soil_health_raw_data_full <- read.csv("~/Desktop/research/UMD_org_soil_MA/Crystal-Ornelas_Thapa_Tully_datasheet - datasheet_v2.csv", header = TRUE)
 
 ## load libraries ####
 library(ggplot2)
 library(cowplot)
 library(maps)
-
 # Create map of world
 head(soil_health_raw_data_full)
 world_map <- map_data("world")
@@ -14,7 +12,9 @@ map_of_study_sites<- ggplot(world_map, aes(x = long, y = lat, group = group)) +
   geom_polygon(fill="#F7F7F7", colour = "black", lwd = .25) +
   geom_point(data = soil_health_raw_data_full, aes(x=longitude, y=latitude) ,color="red", inherit.aes = FALSE) +
   theme_cowplot() +
-  coord_equal(ratio=1)
+  coord_equal(ratio=1) +
+  xlab("longitude") + 
+  ylab("latitude")
 map_of_study_sites
 
 unique_country_and_code<- select(soil_health_raw_data_full, study_code, study_location)
